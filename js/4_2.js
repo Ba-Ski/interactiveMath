aa2f0436b508c036e72cae0ea61312fb7a8141b6
Ba-Ski <color.ui>
2015-11-23 02:52:19 +0300

    making paragraph 4.1

---
+++ js/4_2.js
@@ -0,0 +1,26 @@
+$(function(){
+  $('#btn1').click(function(){
+	  
+	  
+	  	document.getElementById("paths11").classList.remove("firstDrow");
+	  	document.getElementById("paths12").classList.remove("secondDrow");
+	  	document.getElementById("paths21").classList.remove("secondDrow");
+	  	document.getElementById("paths22").classList.remove("firstDrow");
+
+	  	$("#11_22").removeClass("firstAppear");
+		$("#dot").removeClass("middleAppear");
+		$("#21_12").removeClass("secondAppear");
+		
+		setTimeout(function() {
+        document.getElementById("paths11").classList.add("firstDrow");
+		document.getElementById("paths12").classList.add("secondDrow");
+		document.getElementById("paths21").classList.add("secondDrow");
+		document.getElementById("paths22").classList.add("firstDrow");
+		
+		$("#11_22").addClass("firstAppear");
+		$("#dot").addClass("middleAppear");
+		$("#21_12").addClass("secondAppear");
+	  
+    }, 100);
+  });
+});
