69ec428a94a9382438219a74d165157c50290b40
Ba-Ski <color.ui>
2015-12-18 02:04:58 +0300

    fixes

---
+++ js/3_1.js
@@ -0,0 +1,11 @@
+/**
+ * Created by baski on 18.12.15.
+ */
+$(document).ready(function(){
+    $("#flip1").click(function(){
+        $("#panel1").slideToggle("slow");
+    });
+    $("#flip2").click(function(){
+        $("#panel2").slideToggle("slow");
+    });
+});
--- starter-template/js/common.js
+++
@@ -1,7 +0,0 @@
-function addApperanceCSS(name, startTime, duration, fillMode, animation) {
-    var item = document.getElementById(name);
-    item.style.animationDelay = startTime;
-    item.style.animationDuration = duration;
-    item.style.animationName = animation;
-    item.style.animationFillMode = fillMode;
-}
