0c0bb0995f22caf7d730cf7f1efe39fb0e8bd38f
Ba-Ski <color.ui>
2015-12-19 22:19:01 +0300

    missing files were added

---
+++ js/init.js
@@ -0,0 +1,10 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+
+  }); // end of document ready
+  $(document).ready(function(){
+    $('.parallax').parallax();
+  });
+})(jQuery); // end of jQuery name space
